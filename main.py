9e945181671865aadfd0fce5c5ab0cbf2ac3b502
anikatasnin <jarintasnin09@gmail.com>
2025-10-04 00:22:29 +0600

    feat: add helper function to convert PIL images to NumPy arrays

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from PIL import Image, ImageTk
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+
+# helper function
+# convert image to numpy array
+def pil_to_np(img): 
+    return np.array(img)
